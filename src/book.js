692444e6898c86847818071cdfd53cf7d99da287
petetes93 <alejandrolobatohaurie@gmail.com>
2023-11-03 14:49:00 +0100

    first-commit

--- src/book.js
+++ src/book.js
@@ -1,17 +1,21 @@
 const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema({
-	title: String,
-	isbn: String,
-	status: String,
-	pageCount: String,
-	published: Boolean,
-	thumbnailUrl: String,
-	publishedDate: Date,
-	shortDescription: String,
-	longDescription: String,
+	title: {type: String, required: true },
+	isbn: {type: String, required: true , unique: true},
+	status: {type: String, required: true },
+	pageCount: {type: Number},
+	published: {type: Boolean, default: true },
+	thumbnailUrl:  {type: String},
+	publishedDate:  Date,
+	description: {
+			shortDescription: {type: String},
+			longDescription: {type: String},
+		},
+	
+
 	authors: [{ type: String }],
-	categories: { type: String },
+	categories: [{ type: String }],
 });
 
 const Book = mongoose.model('book', bookSchema);
